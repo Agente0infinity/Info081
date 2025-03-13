d228b13629260ea7212bd4789e1421c8c4f7f25f
Alejandro Campos <clasespaesm@gmail.com>
2025-03-13 13:33:40 -0300

    bacano po

--- ejercicios random/strings.py
+++ ejercicios random/strings.py
@@ -1,4 +1,4 @@
-contar_vocales(cadena):
+def contar_vocales(cadena):
     contador_vocales=0
     vocales="aeiouAEIOU"
     for i in cadena:
@@ -9,6 +9,20 @@ contar_vocales(cadena):
 
                 contador_vocales+=1
 
-    return f"hay {contar_vocales} vocales en {cadena}"
+    return f"hay {contador_vocales} vocales en {cadena}"
 
 print(contar_vocales("palabra"))
+
+def invertir_Cadena(cadena):
+
+    anedac=""
+
+    for i in range(len(cadena)):
+        posicion= len(cadena)-i-1
+        anedac+= cadena[posicion]
+
+    return anedac
+
+print(invertir_Cadena("palabra"))
+
+def
