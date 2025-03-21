c2fcb7409024e90aef1d70aaa05424329983d9dd
Alejandro Campos <clasespaesm@gmail.com>
2025-03-21 14:21:27 -0300

    el objeto perro

---
+++ perro.py
@@ -0,0 +1,22 @@
+import random
+import math
+import time
+class Perro:
+    def __init__(self, tamaño, color, raza, ladridos):
+        self.tamaño = tamaño
+        self.color = color
+        self.raza = raza
+        self.ladridos = ladridos  
+
+    def ladrar(self):
+        cantidad=math.floor(random.randint(1,1000))
+        for i in range(cantidad):
+            time.sleep(1)
+            print(math.floor(random.choice(self.ladridos)),end= " ")
+         
+       
+
+yorkshire = Perro("pequeño", "café", "yorkshire", ["guau", "bark", "wulf", "wauf"])
+
+# Hacer que el perro ladre
+print(yorkshire.ladrar())
